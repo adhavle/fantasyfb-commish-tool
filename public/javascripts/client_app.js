85336769ce97cce99051cfbc83885f6c63c75c58
adhavle <8816962+adhavle@users.noreply.github.com>
2025-10-25 17:09:28 -0700

    Wire up UX with data from backend

--- public/javascripts/client_app.js
+++ public/javascripts/client_app.js
@@ -3,22 +3,27 @@ console.log("logging output, after modifying locally")
 $(document).ready(function() {
     console.log("JQuery is loaded");
 
-    // fetch team and player data
-    teams = JSON.parse(document.getElementById("teams_data").innerHTML);
-    players = JSON.parse(document.getElementById("players_data").innerHTML);
+    // Fetch league data
+    leagueData = JSON.parse(document.getElementById("leagueData").innerHTML);
 
-    // add list of teams
+    // Populate list of teams
     var teamsListHtml = "";
-    teams.forEach(team => {
-        teamsListHtml += `<button type="button" onclick="showKeepers(${team['teamId']})" id="team-list-${team['teamId']}" class="btn btn-sm btn-outline-dark text-start">${team['teamName']}</button>`;
+    var style = "btn btn-sm btn-outline-dark text-start";
+
+    Object.keys(leagueData).forEach(teamId => {
+        teamName = leagueData[teamId].teamName;
+        teamLogo = `<img src="${leagueData[teamId].teamLogoUrl}" height="24px" style="padding: 0px 4px; border-radius: 50%;">`
+        teamsListHtml +=`<button type="button" onclick="showKeepers(${teamId})" id="team-list-${teamId}" class="${style}">${teamLogo} ${teamName}</button>`;
     });
+
     $("#list-of-teams").html(teamsListHtml);
-    showKeepers(1);    
+
+    // If wide screen show keepers.
+    showKeepers('1');
 })
 
-var currentTeamId = 1;
+var currentTeamId = '1';
 function showKeepers(teamId) {
-    teamId = parseInt(teamId);
     
     // restore style for current team
     $(`#team-list-${currentTeamId}`).removeClass("list-group-item-info");
@@ -28,11 +33,54 @@ function showKeepers(teamId) {
     currentTeamId = teamId;
     
     // list all players from selected team
-    currentTeamPlayers = players[currentTeamId];
+    currentTeamPlayers = leagueData[currentTeamId].players;
+    currentTeamPlayers.sort((a, b) => { return b.keeperValue - a.keeperValue; });
+
     var playersListHtml = "";
-    currentTeamPlayers.forEach(plr => {
-        playersListHtml += `<li class="list-group-item list-group-item-info">[${plr['position']}] ${plr['fullname']}</li>`;
+    currentTeamPlayers.forEach(p => {
+        keeperPill = getKeeperPill(p.keeperValue);
+        headshot = `<img src="${p.imageUrl}" height="28px" style="padding: 0px 4px;">`
+        position = getPositionPill(p.position);
+        playersListHtml += `<li class="list-group-item list-group-item-info">${position} &nbsp; ${headshot} &nbsp; ${p.name} &nbsp; ${keeperPill}</li>`;
     });
 
+    console.log(playersListHtml);
+
     $("#list-of-keepers").html(playersListHtml);
+}
+
+const getPositionPill = function(pos) {
+    style = "display: inline-block; background-color: blue; color: white; font-weight: bold; font-size: 12px; ";
+    style += "height: 24px; width: 24px; border-radius: 50%; text-align: center; padding: 3px 2px 3px 1px";
+    return `<span style="${style}">${pos}</span>`;
+}
+
+const getKeeperPill = function(keeperValue) {
+    pillColor = keeperValue >= 0 ? "green" : "red";
+    keeperNote = "";
+    switch (keeperValue) {
+        case -5:
+            keeperNote = "Nope";
+            break;
+        case -4:
+            keeperNote = "No Bro";
+            break;
+        case -3:
+            keeperNote = "Late Add";
+            break;
+        case -2:
+            keeperNote = "Keeper Limit";
+            break;
+        case -1:
+            keeperNote = "Drafted R1";
+            break;
+        case 0:
+            keeperNote = "R10 (UDFA)";
+            break;
+        default:
+            keeperNote = `R${keeperValue}`;
+    }
+
+    pillStyle = "padding: 2px 5px 4px 4px; color: white; font-weight: bold; font-size: 12px; border-radius: 5px;";
+    return `<span style="background-color: ${pillColor}; ${pillStyle}">${keeperNote}</span>`;
 }
--- routes/index.js
+++ routes/index.js
@@ -376,65 +376,8 @@ router.get('/', async function(req, res, next) {
         });
       });
 
-      // Fetch team names
-      let response = await fetch(globals.GetTeamsUri, { headers: { Authorization: `Bearer ${globals.getBearerToken()}` } })
-        .then(response => response.text());
-
-      var parsedResponse = undefined;
-      parseString(response, (err, parsed) => {
-        if (err) {
-          console.error(`Error parsing XML: ${err}`);
-        }
-
-        parsedResponse = parsed;
-      });
-
-      teamsInfo = parsedResponse.fantasy_content.league[0]['teams'][0];
-      teamsData = [];
-      teamKeys = [];
-      teamsInfo['team'].forEach(element => {
-        teamsData.push({
-          teamKey: element['team_key'][0],
-          teamId: element['team_id'][0],
-          teamName: `${element['name'][0]} (${element['managers'][0]['manager'][0]['nickname']})` });
-
-        teamKeys.push(element['team_key'][0]);
-      });
-
-      // Fetch rosters
-      let rostersUri = globals.GetRostersUri.replace("TEAMKEYS", teamKeys.join(","));
-      response = await fetch(rostersUri, { headers: { Authorization: `Bearer ${globals.getBearerToken()}` } })
-        .then(response => response.text());
-
-      parsedResponse = undefined;
-      parseString(response, (err, parsed) => {
-        if (err) {
-          console.error(`Error parsing XML: ${err}`);
-        }
-
-        parsedResponse = parsed;
-      });
-
-      globals.log.info(rostersUri);
-      
-      teamsInfo = parsedResponse.fantasy_content['teams'][0];
-      players = {};
-      teamsInfo['team'].forEach(element => {
-
-        players[element['team_id'][0]] = [];
-
-        roster = element['roster'][0]['players'][0];
-        roster['player'].forEach(player => {
-          players[element['team_id'][0]].push({
-            fullname: player['name'][0]['full'][0],
-            position: player['display_position'][0]
-          });
-        });
-      });
-
-
-      // send back UX with backing data.
-      res.render('index', { teams: JSON.stringify(teamsData), players: JSON.stringify(players) });
+      // Render UX with backing data.
+      res.render('index', { leagueData: JSON.stringify(league_rosters) });
   }
 });
 
